a0cfbdf86c8801bda818306d4c4fa3ca3a8b5f2b
DrewDrizzle <aramouni8@gmail.com>
2018-12-18 21:51:37 -0500

    new plugin

--- javascript/app.js
+++ javascript/app.js
@@ -61,6 +61,22 @@ $.ajax({
             '</div>' + 
             '</div>'
         );
+       /* $('.card').plate();
+$('.card').plate({
+
+    // inverse animation
+    inverse: false,
+  
+    // transformation perspective in pixels
+    perspective: 500,
+  
+    // maximum rotation in degrees
+    maxRotation: 10,
+  
+    // duration in milliseconds
+    animationDuration: 200
+    
+  });*/
                     
     }
     geocodeQuery(zipcode);
---
+++ javascript/zoomIn.js
@@ -0,0 +1,16 @@
+$('.card').plate();
+$('.card').plate({
+
+    // inverse animation
+    inverse: false,
+  
+    // transformation perspective in pixels
+    perspective: 500,
+  
+    // maximum rotation in degrees
+    maxRotation: 10,
+  
+    // duration in milliseconds
+    animationDuration: 200
+    
+  });
